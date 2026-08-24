159e526e0a9cb682f5f8a58cec2b40bf5745a227
Hiep Nguyen <73454492+hiepdng@users.noreply.github.com>
2026-08-24 13:22:46 -0700

    Fix typo in Lambda endpoint configuration

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
   endpoints {
-    lamda = "http://localhost:4566"
+    lambda = "http://localhost:4566"
   }
 }
 
